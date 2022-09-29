35de29b61e1257a08add114a2ac0eb0c00550c6a
Your Name <you@example.com>
2022-09-29 19:30:55 +0300

    hahmohuojuuu

--- player.gd
+++ player.gd
@@ -2,6 +2,12 @@ extends KinematicBody
 
 var run_speed : float = 8.0
 
+var time : float = 0.0
+var step_freq : float = 2.0
+var step_height : float = 0.2
+var step_tilt : float = 8.0
+
+onready var boby_Hinge = $BobyHinge
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -17,4 +23,7 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
+	boby_Hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
+	time += delta
+	boby_Hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time)
 	move_and_slide(Vector3(0,0, -run_speed))
