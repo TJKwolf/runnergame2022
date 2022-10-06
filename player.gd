18cef7fe59d997ee58c435f84a79a45f84424e85
Your Name <you@example.com>
2022-10-06 19:31:51 +0300

    seinät tielle

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
 var run_speed : float = 8.0
+var sidestep_speed : float = 5.0
+var velocity := Vector3()
 
 var time : float = 0.0
 var step_freq : float = 2.0
@@ -24,6 +26,19 @@ func _ready():
 
 func _physics_process(delta):
 	boby_Hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
-	time += delta
 	boby_Hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time)
-	move_and_slide(Vector3(0,0, -run_speed))
+	time += delta
+	
+	var sideways : float = 0.0
+	
+	if Input.is_action_pressed("move_right"):
+		sideways += 1.0
+	
+	if Input.is_action_pressed("move_left"):
+		sideways -= 1.0
+	
+	velocity.x = sideways *sidestep_speed
+	velocity.z = -run_speed
+	
+	move_and_slide(velocity)
+
